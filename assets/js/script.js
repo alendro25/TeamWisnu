b88e428090b4b7547f5d9c82d01ed08440e51dc2
alendro25 <alendro25@gmail.com>
2022-08-18 20:10:54 +0700

    First commit

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel();
+});
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 80,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: false,
+    },
+    600: {
+      items: 2,
+      nav: false,
+    },
+    1000: {
+      items: 3,
+      nav: false,
+      loop: true,
+    },
+  },
+});
+
+const scrollTop = document.querySelector(".scrollTop");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    scrollTop.classList.add("active");
+  } else {
+    scrollTop.classList.remove("active");
+  }
+});
